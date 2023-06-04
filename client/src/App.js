cbbef64a473fcfab09def9ac4389f08881a263c3
mahir p c <pcmuhammedmahir@gmail.com>
2023-06-04 20:34:55 +0530

    made connection from client to server using socket

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes ,Route } from 'react-router-dom';
 
 import Join from './components/join/Join';
 import Chat from './components/chat/Chat';
+import "./style.css"
 
 const App = () => {
   return (
--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
+import {useLocation} from 'react-router-dom'
+
+let socket;
 
 const Chat = () => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const ENDPOINT = 'localhost:5000'
+  const location = useLocation();
+  useEffect(()=>{
+    const {name,room}= queryString.parse(location.search)
+
+    socket = io(ENDPOINT,{
+      transports: ['websocket'],
+      upgrade: false,
+    })
+
+    setName(name);
+    setRoom(room);
+  },[])
   return (
     <h1>
       Chat
