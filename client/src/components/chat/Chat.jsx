0b97d1ea2913104183ce820e13da70fa5d8f3f5f
mahir p c <pcmuhammedmahir@gmail.com>
2023-06-06 17:31:50 +0530

    Implemeted sendMessage in client->Chat.jsx

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -3,6 +3,8 @@ import queryString from 'query-string'
 import io from 'socket.io-client'
 import {useLocation} from 'react-router-dom'
 
+import Infobar from '../infobar/Infobar'
+
 let socket;
 
 const Chat = () => {
@@ -35,16 +37,37 @@ const Chat = () => {
   //function for incoming messages
   useEffect(()=>{
     socket.on('message', (message)=>{
-      setMessages(...messages, message)
+      setMessages([...messages, message])
     })
   },[messages])
 
   //function for sending messages
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if(message){
+      socket.emit('sendMessage', message, ()=>setMessage(''))
+    }
+
+    console.log(message, messages);
+  }
   
   return (
-    <h1>
-      Chat
-    </h1>
+    <main className='flex flex-row justify-center items-center'>
+      <div className='flex flex-col justify-start items-center'>
+        <Infobar />
+        <input type='text' 
+          className='border-2 border-gray-400 rounded-md p-2 m-2'
+          value={message} 
+          onChange={(e)=>{setMessage(e.target.value)}}
+          onKeyDown={event => event.key === 'Enter' ? 
+                              sendMessage(event) : 
+                              null
+                    }  
+        />
+
+      </div>
+    </main>
   )
 }
 
