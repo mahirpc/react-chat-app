ebb86167093aabc5f3dc65240db44020d93a8430
mahir p c <pcmuhammedmahir@gmail.com>
2023-06-06 18:49:00 +0530

    Updated style of Input component

--- client/src/components/input/Input.jsx
+++ client/src/components/input/Input.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Input = ({message,setMessage,sendMessage}) => {
   return (
-    <div className='mt-auto pr-4'>
+    <form className='mt-auto flex flex-row w-full'>
       <input type='text' 
-          className='border-2 border-gray-400 rounded-md px-2 m-2
-                  text-black font-semibold text-lg w-full'
+          className=' py-1 px-2
+                  text-black font-semibold text-lg w-full focus:outline-none'
           value={message} 
           onChange={(e)=>{setMessage(e.target.value)}}
           onKeyDown={event => event.key === 'Enter' ? 
@@ -13,7 +13,12 @@ const Input = ({message,setMessage,sendMessage}) => {
                               null
                     }  
         />
-    </div>
+        <button onClick={event => sendMessage(event)} className='bg-white'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" className="bi bi-send-fill mx-3" viewBox="0 0 16 16">
+                <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z"/>
+            </svg>
+        </button>
+    </form>
   )
 }
 
