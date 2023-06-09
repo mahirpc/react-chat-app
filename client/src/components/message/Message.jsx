1c77ac2fbc744366a88453170dc9044909abaf6a
mahir p c <pcmuhammedmahir@gmail.com>
2023-06-09 19:44:48 +0530

    Implemented ReactEmoji

--- client/src/components/message/Message.jsx
+++ client/src/components/message/Message.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactEmoji from 'react-emoji'
 
 const Message = ({message:{user,text}, name}) => {
     let isSentByCurrentUser = false;
@@ -15,14 +16,14 @@ const Message = ({message:{user,text}, name}) => {
                 {trimmedName}
             </p>
             <div className=' bg-blue-500'>
-                <p className=' text-white'>{text}</p>
+                <p className=' text-white'>{ReactEmoji.emojify(text)}</p>
             </div>
         </div>
     ) : 
     (
         <div className='flex flex-row justify-start'>
             <div className=' bg-slate-200'>
-                <p className='text-black'>{text}</p>
+                <p className='text-black'>{ReactEmoji.emojify(text)}</p>
             </div>
             <p className='pl-10'>{user}</p>
         </div>
