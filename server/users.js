e42a3ecad7b1c4aebf92ff94ae55f7cd1db20b36
mahir p c <pcmuhammedmahir@gmail.com>
2023-06-09 19:46:10 +0530

    edited a bug

--- server/users.js
+++ server/users.js
@@ -16,11 +16,9 @@ const addUser = ({ id, name, room}) => {
     
 
 }
-const removeUser = ({ id }) => {
-    const index = users.findIndex((user) => user.id == id);
-    if(index != -1){
-        return users.splice(index, 1)[0];
-    }
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id);
+    if(index != -1)  return users.splice(index, 1)[0];
 }
 const getUser = (id) => users.find((user)=>user.id === id);
 const getUsersInRoom = (room) => users.filter((user) => user.room === room);
