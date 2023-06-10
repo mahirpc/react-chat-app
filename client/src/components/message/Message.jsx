1eba20a2c7c714e21df2d01f78883292a7eece47
mahir p c <pcmuhammedmahir@gmail.com>
2023-06-10 07:59:40 +0530

    different styles for infos from admin

--- client/src/components/message/Message.jsx
+++ client/src/components/message/Message.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import ReactEmoji from 'react-emoji'
 
 const Message = ({message:{user,text}, name}) => {
-    let isSentByCurrentUser = false;
+    let sentBy = "";
     const trimmedName = name.trim().toLowerCase();
 
     if(user === trimmedName){
-        isSentByCurrentUser = true;
+        sentBy = "own";
+    } else if (user === "admin"){
+        sentBy = "admin";
+    } else {
+        sentBy = "other";
     }
-
   return (
-    isSentByCurrentUser ? (
+    sentBy === "own" ? (
         <div className='flex flex-row justify-end'>
             <p className='pr-10'>
                 {trimmedName}
@@ -19,13 +22,19 @@ const Message = ({message:{user,text}, name}) => {
                 <p className=' text-white'>{ReactEmoji.emojify(text)}</p>
             </div>
         </div>
-    ) : 
-    (
+    ) : sentBy === "other" ? (
         <div className='flex flex-row justify-start'>
             <div className=' bg-slate-200'>
                 <p className='text-black'>{ReactEmoji.emojify(text)}</p>
             </div>
             <p className='pl-10'>{user}</p>
+        </div>
+    ) : (
+        <div className='flex flex-row justify-center'>
+            <div className=' bg-slate-500'>
+                <p className=' text-slate-800'>{ReactEmoji.emojify(text)}</p>
+            </div>
+            <p className=''>{user}</p>
         </div>
     )
   )
